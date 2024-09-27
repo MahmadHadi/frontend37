7d22f4ad8a1592fd46aa5d35ef8682e9e7aae215
MahmadHadi <nayanihadi97@gmail.com>
2024-09-27 19:46:47 +0530

    27 sep

---
+++ QRCode/script.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector(".btn");
+const img = document.querySelector("img");
+const link = document.querySelector(".qr-input");
+
+const api = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example`;
+
+btn.addEventListener("click", async () => {
+    if (link.value == "") {
+        alert("Invalid input ");
+        return;
+    }
+
+    // const data = fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=` + link.value)
+    const data = await fetch(
+        `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${link.value}`
+    );
+    // console.log(data.url);
+
+    img.src = data.url;
+});
